3b4d6c76674ed0703fc0302d7789a3332d2605c4
Günay Coşkun <61664535+gunaycoskun@users.noreply.github.com>
2022-08-05 00:58:07 +0300

    Update Program.cs

--- ProjectWatchDog/ProjectWatchDog/Program.cs
+++ ProjectWatchDog/ProjectWatchDog/Program.cs
@@ -4,7 +4,7 @@ using WatchDog.src.Enums;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+//https://github.com/IzyPro/WatchDog
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
